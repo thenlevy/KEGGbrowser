ffb07ff89cc994ac164888da48519b1bb303ed55
Hassene Ben yedder <benyedder.hassene@yahoo.com>
2018-04-11 18:49:45 +0200

    add text function

--- src/KEGG.java
+++ src/KEGG.java
@@ -3,34 +3,36 @@ import java.net.*;
  
 public class KEGG{
 	public static String get_genome_data(String specie, String gene_id) {
-		
 		String url_name = ("http://rest.kegg.jp/get/"+ specie+ ":" 
-						  + gene_id);
+							+ gene_id);
+		return get_text_url(url_name);
+
+	}
+	private static String get_text_url(String url_name){
 		String ret= "";				  
 		try{
-		    URL url = new URL(url_name);
-		
-		    URLConnection path =url.openConnection();
-		    System.out.println(path.getContent());
-		    InputStream input = path.getInputStream();
-		    
-		
-		    while(input.available()>0)
-		    ret += (char)input.read();
+			URL url = new URL(url_name);
+
+			URLConnection path =url.openConnection();
+			System.out.println(path.getContent());
+			InputStream input = path.getInputStream();
+
+
+			while(input.available()>0)
+			ret += (char)input.read();
 		}
 		catch(MalformedURLException e){
-		System.out.println(e);
-	}
-	    catch(IOException e){
-		    System.out.println(e);
+			System.out.println(e);
+		}
+		catch(IOException e){
+			System.out.println(e);
+		}
+		return ret;
 		
 	}
-	return ret;
-			
-	}
-public static void main(String[] argv) {
+	public static void main(String[] argv) {
 	
-	String test = get_genome_data("eco" , "b0630");
-	System.out.println(test);
-}
+		String test = get_genome_data("eco" , "b0630");
+		System.out.println(test);
+	}
 }
