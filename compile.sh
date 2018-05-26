ea485ea36e146284cb6d0144f559a231a53f364d
Nicolas <nicolaspierrelevy@gmail.com>
2018-05-26 10:18:01 +0000

    Add download instruction to compile script

--- compile.sh
+++ compile.sh
@@ -1,1 +1,5 @@
+if ! [ -e swingbox-bin.jar ]
+then
+    wget https://sourceforge.net/projects/cssbox/files/SwingBox/1.1/swingbox-1.1-bin.jar/download -O swingbox-bin.jar
+fi
 javac -cp src:swingbox-bin.jar src/Browser.java
